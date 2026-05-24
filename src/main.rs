e8212684c3cf029b2f912193993299c193baf889
elysia <3302843873@qq.com>
2026-05-24 11:53:44 +0800

    draws a line on the fb

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@
 
 use core::panic::PanicInfo;
 
+use framebuffer_draw::{Color, Framebuffer};
 use roxy_loader_api::{bootinfo::BootInfo, kernel_entry};
 
 kernel_entry!(kernel_main);
 
 fn kernel_main(bootinfo: &BootInfo) -> ! {
-    unsafe {
-        let ptr = bootinfo.framebuffer.ptr();
+    let framebuffer: Framebuffer = bootinfo.framebuffer.into();
 
-        for i in 0..bootinfo.framebuffer.size {
-            ptr.add(i).write_volatile(69);
-        }
+    let size = core::cmp::min(framebuffer.width, framebuffer.height);
+
+    for i in 0..size {
+        framebuffer.draw_pixel(i, i, Color::rgb(255, 255, 255));
     }
 
     loop {}
